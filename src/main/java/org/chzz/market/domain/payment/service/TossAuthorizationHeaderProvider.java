5b15a378ed89f8795ec9b479fcf59646afc50340
YeaChan05 <qkenrdl05@gmail.com>
2024-07-23 21:16:24 +0900

    [refactor] 인증 헤더 발급 로직 수정 (#CHZZ-27)
    secret key를 추상 클래스로 이전

--- src/main/java/org/chzz/market/domain/payment/service/TossAuthorizationHeaderProvider.java
+++ src/main/java/org/chzz/market/domain/payment/service/TossAuthorizationHeaderProvider.java
@@ -2,24 +2,14 @@ package org.chzz.market.domain.payment.service;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public class TossAuthorizationHeaderProvider extends PaymentAuthorizationHeaderProvider {
     private static final String PREFIX = "Basic ";
-
-    private final String secretKey;
-    private final String clientKey;
-
-    public TossAuthorizationHeaderProvider(
-            @Value("${payment.toss.security-key}")
-            String secretKey,
-            @Value("${payment.toss.client-key}")
-            String clientKey) {
-        this.secretKey = secretKey;
-        this.clientKey = clientKey;
+    public TossAuthorizationHeaderProvider(String secretKey, String clientKey) {
+        super(secretKey, clientKey);
     }
+
 
     @Override
     public String getAuthorizationHeader() {
