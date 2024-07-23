2e4d046253a3dadc535dcdcd2477f3cb9c268859
junest66 <chlwndks333@gmail.com>
2024-07-23 23:03:58 +0900

    test: 기본 Jpa 에서 Spring Data Jpa 사용으로 테스트 코드 변경
    (#28)

--- src/test/java/org/chzz/market/domain/auction/repository/AuctionRepositoryImplTest.java
+++ src/test/java/org/chzz/market/domain/auction/repository/AuctionRepositoryImplTest.java
@@ -4,15 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import org.chzz.market.common.DatabaseTest;
 import org.chzz.market.domain.auction.dto.AuctionResponse;
 import org.chzz.market.domain.auction.entity.Auction;
+import org.chzz.market.domain.auction.entity.SortType;
 import org.chzz.market.domain.bid.entity.Bid;
+import org.chzz.market.domain.bid.repository.BidRepository;
 import org.chzz.market.domain.image.entity.Image;
+import org.chzz.market.domain.image.repository.ImageRepository;
 import org.chzz.market.domain.product.entity.Product;
 import org.chzz.market.domain.product.entity.Product.Category;
-import org.chzz.market.domain.auction.entity.SortType;
+import org.chzz.market.domain.product.repository.ProductRepository;
 import org.chzz.market.domain.user.entity.User;
+import org.chzz.market.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -32,6 +37,18 @@ class AuctionRepositoryImplTest {
     @Autowired
     AuctionRepository auctionRepository;
 
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ImageRepository imageRepository;
+
+    @Autowired
+    BidRepository bidRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -39,17 +56,12 @@ class AuctionRepositoryImplTest {
     void setUp() {
         User user1 = User.builder().providerId("1234").nickname("닉네임1").email("asd@naver.com").build();
         User user2 = User.builder().providerId("12345").nickname("닉네임2").email("asd1@naver.com").build();
-
-        entityManager.persist(user1);
-        entityManager.persist(user2);
+        userRepository.saveAll(List.of(user1, user2));
 
         Product product1 = Product.builder().user(user1).name("제품1").category(Category.FASHION).build();
         Product product2 = Product.builder().user(user1).name("제품2").category(Category.BOOK).build();
         Product product3 = Product.builder().user(user2).name("제품3").category(Category.FASHION).build();
-
-        entityManager.persist(product1);
-        entityManager.persist(product2);
-        entityManager.persist(product3);
+        productRepository.saveAll(List.of(product1, product2, product3));
 
         Auction auction1 = Auction.builder().product(product1).minPrice(1000L).status(Auction.Status.PROCEEDING)
                 .build();
@@ -57,32 +69,19 @@ class AuctionRepositoryImplTest {
                 .build();
         Auction auction3 = Auction.builder().product(product3).minPrice(3000L).status(Auction.Status.PROCEEDING)
                 .build();
+        auctionRepository.saveAll(List.of(auction1, auction2, auction3));
 
-        entityManager.persist(auction1);
-        entityManager.persist(auction2);
-        entityManager.persist(auction3);
-
-        // 이미지 추가
         Image image1 = Image.builder().product(product1).cdnPath("path/to/image1_1.jpg").build();
         Image image2 = Image.builder().product(product1).cdnPath("path/to/image1_2.jpg").build();
         Image image3 = Image.builder().product(product2).cdnPath("path/to/image2.jpg").build();
         Image image4 = Image.builder().product(product3).cdnPath("path/to/image3.jpg").build();
-
-        entityManager.persist(image1);
-        entityManager.persist(image2);
-        entityManager.persist(image3);
-        entityManager.persist(image4);
+        imageRepository.saveAll(List.of(image1, image2, image3, image4));
 
         Bid bid1 = Bid.builder().bidder(user1).auction(auction1).amount(2000L).count(3).build();
         Bid bid2 = Bid.builder().bidder(user2).auction(auction1).amount(3000L).count(1).build();
         Bid bid3 = Bid.builder().bidder(user1).auction(auction2).amount(4000L).count(2).build();
         Bid bid4 = Bid.builder().bidder(user2).auction(auction3).amount(5000L).count(4).build();
-
-        entityManager.persist(bid1);
-        entityManager.persist(bid2);
-        entityManager.persist(bid3);
-        entityManager.persist(bid4);
-        entityManager.flush();
+        bidRepository.saveAll(List.of(bid1, bid2, bid3, bid4));
     }
 
     @AfterEach
