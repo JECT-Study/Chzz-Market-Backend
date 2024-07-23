1654ead985f7c9395e4ad9c8343581b34efd475c
YeaChan05 <qkenrdl05@gmail.com>
2024-07-23 21:22:33 +0900

    [fix] 결제 승인 요청 객체 수정 (#CHZZ-27)
    주문번호를 요청에 담을 수 있도록 수정

--- src/main/java/org/chzz/market/domain/payment/dto/request/ApprovalRequest.java
+++ src/main/java/org/chzz/market/domain/payment/dto/request/ApprovalRequest.java
@@ -1,6 +1,5 @@
 package org.chzz.market.domain.payment.dto.request;
-
 public record ApprovalRequest(String paymentKey,
-                              String orderId,
-                              Long amount) {
+                              Long amount,
+                              String orderId) {
 }
