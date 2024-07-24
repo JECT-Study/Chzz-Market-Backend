b4aba5b81d653228b1fe95ed2fab4740986cff92
junest66 <chlwndks333@gmail.com>
2024-07-25 00:05:14 +0900

    refactor: 컨트롤러 /api/v1 엔드포인트 제거
    (#33)

--- src/main/java/org/chzz/market/domain/auction/controller/AuctionController.java
+++ src/main/java/org/chzz/market/domain/auction/controller/AuctionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/auctions")
+@RequestMapping("/auctions")
 public class AuctionController {
     private final AuctionService auctionService;
 
