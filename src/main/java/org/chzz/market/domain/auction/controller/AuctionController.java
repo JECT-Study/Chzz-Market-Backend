19eda3a933dfcb20c768daf90a53bd25f60a2853
junest66 <chlwndks333@gmail.com>
2024-07-26 22:43:42 +0900

    feat: 경매 상세정보를 조회하는 API 추가
    (#33)

--- src/main/java/org/chzz/market/domain/auction/controller/AuctionController.java
+++ src/main/java/org/chzz/market/domain/auction/controller/AuctionController.java
@@ -1,12 +1,13 @@
 package org.chzz.market.domain.auction.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.chzz.market.domain.auction.entity.SortType;
 import org.chzz.market.domain.auction.service.AuctionService;
 import org.chzz.market.domain.product.entity.Product.Category;
-import org.chzz.market.domain.auction.entity.SortType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class AuctionController {
                                             @RequestParam(defaultValue = "newest") SortType type,
                                             Pageable pageable) {
         return ResponseEntity.ok(auctionService.getAuctionListByCategory(category, type, 1L, pageable)); // 임의의 사용자 ID
+    }
+
+    @GetMapping("/{auctionId}")
+    public ResponseEntity<?> getAuctionDetails(@PathVariable Long auctionId) {
+        return ResponseEntity.ok(auctionService.getAuctionDetails(auctionId, 1L)); // TODO: 추후에 인증된 사용자 정보로 수정 필요
     }
 }
