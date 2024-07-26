9c27e51372648e126e54c7a4b4c70a7f91f173d7
junest66 <chlwndks333@gmail.com>
2024-07-26 22:33:55 +0900

    feat: 경매 상세정보를 조회하는 서비스 함수 추가
    (#33)

--- src/main/java/org/chzz/market/domain/auction/service/AuctionService.java
+++ src/main/java/org/chzz/market/domain/auction/service/AuctionService.java
@@ -1,10 +1,15 @@
 package org.chzz.market.domain.auction.service;
 
+import static org.chzz.market.domain.auction.error.AuctionErrorCode.AUCTION_NOT_ACCESSIBLE;
+
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.chzz.market.domain.auction.dto.AuctionDetailsResponse;
 import org.chzz.market.domain.auction.dto.AuctionResponse;
+import org.chzz.market.domain.auction.entity.SortType;
+import org.chzz.market.domain.auction.error.AuctionException;
 import org.chzz.market.domain.auction.repository.AuctionRepository;
 import org.chzz.market.domain.product.entity.Product.Category;
-import org.chzz.market.domain.auction.entity.SortType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,4 +25,10 @@ public class AuctionService {
                                                           Pageable pageable) {
         return auctionRepository.findAuctionsByCategory(category, sortType, userId, pageable);
     }
+
+    public AuctionDetailsResponse getAuctionDetails(Long auctionId, Long userId) {
+        Optional<AuctionDetailsResponse> auctionDetails = auctionRepository.findAuctionDetailsById(auctionId, userId);
+        return auctionDetails.orElseThrow(() -> new AuctionException(AUCTION_NOT_ACCESSIBLE));
+    }
+
 }
