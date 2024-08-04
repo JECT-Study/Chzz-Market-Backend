2ac9e8d3e565cca630563fde20b211e6bb1a49ef
junest66 <chlwndks333@gmail.com>
2024-08-04 16:44:30 +0900

    feat: Notification 커스텀 에러 추가
    (#48)

---
+++ src/main/java/org/chzz/market/domain/notification/error/NotificationErrorCode.java
@@ -0,0 +1,27 @@
+package org.chzz.market.domain.notification.error;
+
+import static org.springframework.http.HttpStatus.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.chzz.market.common.error.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum NotificationErrorCode implements ErrorCode {
+    REDIS_MESSAGE_SEND_FAILURE(INTERNAL_SERVER_ERROR, "Redis 메시지 발신에 실패했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
+}
