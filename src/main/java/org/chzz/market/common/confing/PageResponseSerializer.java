fc56ed8a46abeeb82512c5f57e29ddacbe9bcb19
YeaChan05 <qkenrdl05@gmail.com>
2024-08-04 19:02:49 +0900

    [fix] 페이징 예외처리 구현 (#CHZZ-44)
    페이징 결과가 없는 경우 예외처리 구현

--- src/main/java/org/chzz/market/common/confing/PageResponseSerializer.java
+++ src/main/java/org/chzz/market/common/confing/PageResponseSerializer.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import org.chzz.market.common.dto.PageResponse;
+import org.chzz.market.common.error.GlobalErrorCode;
+import org.chzz.market.common.error.GlobalException;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.data.domain.Page;
 
@@ -13,6 +15,9 @@ public class PageResponseSerializer<T> extends JsonSerializer<Page<T>> {
 
     @Override
     public void serialize(Page<T> page, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if(page.isEmpty()){
+            throw new GlobalException(GlobalErrorCode.RESOURCE_NOT_FOUND);
+        }
         PageResponse<T> response = PageResponse.from(page);
         gen.writeObject(response);
     }
