d1bd2e8efc895d14d1e195655f178a6cc8df724d
JongHyunJung <jjh19960115@gmail.com>
2024-08-11 08:39:05 +0900

    refactor: 상품 등록 API 수정 (#CHZZ-35) - 사전 등록, 경매 등록 엔드포인트 하나로 통합 - 경매 상품으로 전환 API 수정

--- src/main/java/org/chzz/market/domain/auction/controller/AuctionController.java
+++ src/main/java/org/chzz/market/domain/auction/controller/AuctionController.java
@@ -1,10 +1,19 @@
 package org.chzz.market.domain.auction.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.chzz.market.domain.auction.dto.RegisterRequest;
+import org.chzz.market.domain.auction.dto.RegisterResponse;
+import org.chzz.market.domain.auction.dto.StartResponse;
 import org.chzz.market.domain.auction.service.AuctionService;
+import org.chzz.market.domain.auction.service.RegisterService;
 import org.chzz.market.domain.product.entity.Product.Category;
 import org.chzz.market.domain.auction.entity.SortType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +22,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auctions")
 public class AuctionController {
+    private static final Logger logger = LoggerFactory.getLogger(AuctionController.class);
+
     private final AuctionService auctionService;
+    private final RegisterService registerService;
 
     @GetMapping
     public ResponseEntity<?> getAuctionList(@RequestParam Category category,
@@ -26,5 +38,24 @@ public class AuctionController {
     @GetMapping("/{auctionId}")
     public ResponseEntity<?> getAuctionDetails(@PathVariable Long auctionId) {
         return ResponseEntity.ok(auctionService.getAuctionDetails(auctionId, 1L)); // TODO: 추후에 인증된 사용자 정보로 수정 필요
+    }
+
+    // 상품 등록
+    @PostMapping(value = "/register",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RegisterResponse> createAuction(@ModelAttribute @Valid RegisterRequest request) {
+
+        RegisterResponse response = registerService.register(request);
+        logger.info("상품이 성공적으로 등록되었습니다. 상품 ID: {}", response.productId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // 경매 상품으로 전환
+    @PostMapping("/{auctionId}/start")
+    public ResponseEntity<StartResponse> startAuction(@PathVariable Long auctionId) {
+        StartResponse response = auctionService.startAuction(auctionId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
