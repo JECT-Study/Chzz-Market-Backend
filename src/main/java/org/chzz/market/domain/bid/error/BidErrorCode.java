e9bae6b8c69e17a7fb591d57de63fc7a7b015a8d
junest66 <chlwndks333@gmail.com>
2024-08-10 14:00:47 +0900

    feat: 입찰 취소를 할때 발생하는 에러 코드 추가
    (#71)

--- src/main/java/org/chzz/market/domain/bid/error/BidErrorCode.java
+++ src/main/java/org/chzz/market/domain/bid/error/BidErrorCode.java
@@ -10,9 +10,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum BidErrorCode implements ErrorCode {
+    BID_NOT_FOUND(NOT_FOUND, "해당 입찰을 찾을 수 없습니다."),
     BID_BELOW_MIN_PRICE(BAD_REQUEST, "입찰 금액이 최소 금액보다 낮습니다."),
     BID_LIMIT_EXCEEDED(BAD_REQUEST, "입찰 횟수 제한을 초과하여 더 이상 입찰할 수 없습니다."),
-    BID_BY_OWNER(FORBIDDEN, "경매 등록자는 입찰할 수 없습니다.");
+    BID_ALREADY_CANCELLED(BAD_REQUEST, "해당 입찰은 이미 취소되었습니다."),
+    BID_BY_OWNER(FORBIDDEN, "경매 등록자는 입찰할 수 없습니다."),
+    BID_NOT_ACCESSIBLE(FORBIDDEN, "해당 입찰에 접근할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
