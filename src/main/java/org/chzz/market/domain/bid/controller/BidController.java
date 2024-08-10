a14c826a4af91d962946b2bbfa6dc4b61a2a32d7
junest66 <chlwndks333@gmail.com>
2024-08-10 14:06:39 +0900

    feat: 입찰 취소 API 추가
    (#71)

--- src/main/java/org/chzz/market/domain/bid/controller/BidController.java
+++ src/main/java/org/chzz/market/domain/bid/controller/BidController.java
@@ -5,9 +5,9 @@ import static org.springframework.http.HttpStatus.CREATED;
 import lombok.RequiredArgsConstructor;
 import org.chzz.market.domain.bid.dto.BidCreateRequest;
 import org.chzz.market.domain.bid.service.BidService;
-import org.chzz.market.domain.user.entity.User;
-import org.chzz.market.domain.user.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +24,12 @@ public class BidController {
 //                                       @AuthenticationPrincipal CustomUserDetails customUserDetails) // TODO: 추후에 인증된 사용자 정보로 수정 필요
         bidService.createBid(bidCreateRequest, 1L);
         return ResponseEntity.status(CREATED).build();
+    }
+
+    @PatchMapping("/{bidId}/cancel")
+    public ResponseEntity<?> cancelBid(@PathVariable Long bidId) {
+        //                                       @AuthenticationPrincipal CustomUserDetails customUserDetails) // TODO: 추후에 인증된 사용자 정보로 수정 필요
+        bidService.cancelBid(bidId, 2L); // TODO: 추후에 인증된 사용자 정보로 수정 필요
+        return ResponseEntity.ok().build();
     }
 }
