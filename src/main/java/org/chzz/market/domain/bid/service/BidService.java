729f8a2fada525c9d4d788075a5cab9133cc50bf
junest66 <chlwndks333@gmail.com>
2024-08-11 16:40:42 +0900

    refactor: 입찰의 소유자 검사 함수 메소드로 분리 및 로깅 추가
    (#71)

--- src/main/java/org/chzz/market/domain/bid/service/BidService.java
+++ src/main/java/org/chzz/market/domain/bid/service/BidService.java
@@ -1,6 +1,5 @@
 package org.chzz.market.domain.bid.service;
 
-import static org.chzz.market.domain.auction.error.AuctionErrorCode.AUCTION_ENDED;
 import static org.chzz.market.domain.bid.error.BidErrorCode.BID_BELOW_MIN_PRICE;
 import static org.chzz.market.domain.bid.error.BidErrorCode.BID_BY_OWNER;
 import static org.chzz.market.domain.bid.error.BidErrorCode.BID_NOT_ACCESSIBLE;
@@ -8,8 +7,8 @@ import static org.chzz.market.domain.bid.error.BidErrorCode.BID_NOT_FOUND;
 import static org.chzz.market.domain.user.error.UserErrorCode.USER_NOT_FOUND;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.chzz.market.domain.auction.entity.Auction;
-import org.chzz.market.domain.auction.error.AuctionException;
 import org.chzz.market.domain.auction.service.AuctionService;
 import org.chzz.market.domain.bid.dto.BidCreateRequest;
 import org.chzz.market.domain.bid.entity.Bid;
@@ -24,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class BidService {
     private final AuctionService auctionService;
     private final BidRepository bidRepository;
@@ -48,30 +48,33 @@ public class BidService {
         User user = userRepository.findById(userId).orElseThrow(() -> new UserException(USER_NOT_FOUND));
         Bid bid = bidRepository.findById(bidId).orElseThrow(() -> new BidException(BID_NOT_FOUND));
         Auction auction = bid.getAuction();
-        if (bid.getBidder() != user) {
-            throw new BidException(BID_NOT_ACCESSIBLE);
-        }
-        validateAuctionEndTime(auction);
+        validateBidOwnership(user, bid);
+        auction.validateAuctionEndTime();
         bid.cancelBid();
+        log.info("입찰이 취소되었습니다. 입찰 ID: {}, 사용자 ID: {}, 경매 ID: {}", bid.getId(), user.getId(), auction.getId());
     }
-
 
     private void validateBidConditions(BidCreateRequest bidCreateRequest, Long userId, Auction auction) {
         // 경매 등록자가 입찰할 때
         if (auction.getProduct().getUser().getId() == userId) {
             throw new BidException(BID_BY_OWNER);
         }
-        validateAuctionEndTime(auction);
+        auction.validateAuctionEndTime();
         // 최소 금액보다 낮은 금액일 때
         if (!auction.isAboveMinPrice(bidCreateRequest.getAmount())) {
             throw new BidException(BID_BELOW_MIN_PRICE);
         }
     }
 
-    private void validateAuctionEndTime(Auction auction) {
-        // 경매가 진행중이 아닐 때
-        if (!auction.isProceeding() || auction.isEnded()) {
-            throw new AuctionException(AUCTION_ENDED);
+    /**
+     * 입찰의 소유자가 맞는지 확인합니다.
+     *
+     * @param bid  확인할 입찰
+     * @param user 현재 사용자
+     */
+    private void validateBidOwnership(User user, Bid bid) {
+        if (bid.getBidder() != user) {
+            throw new BidException(BID_NOT_ACCESSIBLE);
         }
     }
 }
