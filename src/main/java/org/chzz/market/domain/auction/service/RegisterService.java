f798209a2903b3c09beec9a5fcb385d7d27a1c18
JongHyunJung <jjh19960115@gmail.com>
2024-08-11 10:20:23 +0900

    refactor: 상품 등록 서비스 함수 수정 (#CHZZ-35) - 경매 상태 매개변수 제거 - 주석, 로그 및 TODO 추가

--- src/main/java/org/chzz/market/domain/auction/service/RegisterService.java
+++ src/main/java/org/chzz/market/domain/auction/service/RegisterService.java
@@ -4,8 +4,6 @@ package org.chzz.market.domain.auction.service;
 import org.chzz.market.domain.auction.dto.RegisterRequest;
 import org.chzz.market.domain.auction.dto.RegisterResponse;
 
-import static org.chzz.market.domain.auction.entity.Auction.*;
-
 public interface RegisterService {
-    RegisterResponse register(RegisterRequest request, AuctionStatus InitialStatus);
+    RegisterResponse register(RegisterRequest request);
 }
--- src/main/java/org/chzz/market/domain/auction/service/RegisterServiceImpl.java
+++ src/main/java/org/chzz/market/domain/auction/service/RegisterServiceImpl.java
@@ -12,6 +12,8 @@ import org.chzz.market.domain.user.entity.User;
 import org.chzz.market.domain.user.error.UserErrorCode;
 import org.chzz.market.domain.user.error.exception.UserException;
 import org.chzz.market.domain.user.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,24 +25,38 @@ import static org.chzz.market.domain.auction.entity.Auction.*;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class RegisterServiceImpl implements RegisterService{
+
+    private static final Logger logger = LoggerFactory.getLogger(RegisterServiceImpl.class);
+
     private final ProductRepository productRepository;
     private final AuctionRepository auctionRepository;
     private final ImageService imageService;
     private final UserRepository userRepository;
 
+    /**
+     * 상품 등록 (사전 등록 & 경매 등록)
+     */
     @Override
     @Transactional
-    public RegisterResponse register(RegisterRequest request, AuctionStatus initialStatus) {
+    public RegisterResponse register(RegisterRequest request) {
+        // 유저 유효성 검사
         User user = userRepository.findById(request.getUserId())
-                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> {
+                    logger.info("유저 ID {}번에 해당하는 유저를 찾을 수 없습니다.", request.getUserId());
+                    return new UserException(UserErrorCode.USER_NOT_FOUND);
+                });
 
-//        Product product = request.toProductEntity();
+        // 상품 테이블 등록
+        // Product product = request.toProductEntity();
         Product product = createProduct(request, user);
         productRepository.save(product);
+        logger.info("상품이 상품 테이블에 저장되었습니다. 상품 ID : {}", product.getId());
 
-//        Auction auction = request.toAuctionEntity(product, initialStatus);
-        Auction auction = createAuction(product, request, initialStatus);
+        // 경매 테이블 등록
+        // Auction auction = request.toAuctionEntity(product, initialStatus);
+        Auction auction = createAuction(product, request, request.getStatus());
         auctionRepository.save(auction);
+        logger.info("상품이 경매 테이블에 저장되었습니다. 최종 경매 상태 : {}", auction.getStatus());
 
         // 이미지 처리
         if (request.getImages() != null && !request.getImages().isEmpty()) {
@@ -48,9 +64,10 @@ public class RegisterServiceImpl implements RegisterService{
             imageService.saveProductImageEntities(product, imageUrls);
         }
 
-        return new RegisterResponse(product.getId(), auction.getId(), auction.getStatus(), "success");
+        return RegisterResponse.of(product.getId(), auction.getId(), auction.getStatus());
     }
 
+    // TODO: createProduct, createAuction 메서드 추후 로그인 기능 병합 이후 toEntity 로 수정
     private Product createProduct(RegisterRequest request, User user) {
         return Product.builder()
                 .user(user)
