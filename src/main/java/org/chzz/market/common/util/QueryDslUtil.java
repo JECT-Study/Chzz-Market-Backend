975b1d8e34ee493f3bb90e20b5c8812b7e7d7cfb
junest66 <chlwndks333@gmail.com>
2024-08-12 18:00:28 +0900

    style: deprecated 주석 추가
    (#61)

--- src/main/java/org/chzz/market/common/util/QueryDslUtil.java
+++ src/main/java/org/chzz/market/common/util/QueryDslUtil.java
@@ -13,6 +13,7 @@ public class QueryDslUtil {
      * @param qClass 정렬할 엔티티를 나타내는 QueryDSL 엔티티 경로 클래스입니다.
      * @param sorts  정렬 속성과 방향을 포함하는 Spring Data Sort 객체입니다.
      * @return QueryDSL OrderSpecifier 객체 배열을 반환합니다.
+     * @deprecated
      */
     public static <T> OrderSpecifier<?>[] getOrderSpecifiers(EntityPathBase<T> qClass, Sort sorts) {
         return sorts.stream()
