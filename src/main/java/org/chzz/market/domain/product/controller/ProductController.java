6cbb4c100f34b51bfd228e673fe9c23f52be00f4
JongHyunJung <jjh19960115@gmail.com>
2024-08-14 06:56:05 +0900

    feat: 상품 삭제 및 경매 취소 API 구현 (#CHZZ-51)

--- src/main/java/org/chzz/market/domain/product/controller/ProductController.java
+++ src/main/java/org/chzz/market/domain/product/controller/ProductController.java
@@ -1,6 +1,10 @@
 package org.chzz.market.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.chzz.market.domain.product.dto.DeleteProductResponse;
+import org.chzz.market.domain.product.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,5 +14,16 @@ import org.slf4j.LoggerFactory;
 @RequestMapping("/products")
 public class ProductController {
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    private final ProductService productService;
+
+    @PostMapping("/{productId}/delete")
+    public ResponseEntity<DeleteProductResponse> deleteProduct(
+            @PathVariable Long productId,
+            @RequestHeader("X-User-Agent") Long userId) {
+        DeleteProductResponse response = productService.deleteProduct(productId, userId);
+        logger.info("상품이 성공적으로 삭제되었습니다. 상품 ID: {}", productId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
 }
