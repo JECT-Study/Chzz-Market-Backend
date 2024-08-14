ce5cf1f403692a5fc8d72f47b92891050a10b868
JongHyunJung <jjh19960115@gmail.com>
2024-08-14 23:17:37 +0900

    feat: 사전 등록 상품 상세 정보 조회 API 구현 (#CHZZ-74)

--- src/main/java/org/chzz/market/domain/product/controller/ProductController.java
+++ src/main/java/org/chzz/market/domain/product/controller/ProductController.java
@@ -1,8 +1,11 @@
 package org.chzz.market.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.chzz.market.domain.product.dto.ProductDetailsResponse;
+import org.chzz.market.domain.product.dto.ProductResponse;
 import org.chzz.market.domain.product.entity.Product.Category;
 import org.chzz.market.domain.product.service.ProductService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +20,25 @@ public class ProductController {
 
     private final ProductService productService;
 
-    /**
-     * 사전 등록 상품 목록 조회
+    /*
+     * 카테고리 별 사전 등록 상품 목록 조회
      */
     // TODO: 추후에 인증된 사용자 정보로 수정 필요
     @GetMapping
-    public ResponseEntity<?> getProductList(
+    public ResponseEntity<Page<ProductResponse>> getProductList(
             @RequestParam Category category,
-//            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestHeader("X-User-Agent") Long userId,
             Pageable pageable) {
         return ResponseEntity.ok(productService.getProductListByCategory(category, userId, pageable)); // 임의의 사용자 ID
+    }
+
+    /*
+     * 사전 등록 상품 상세 정보 조회
+     */
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductDetailsResponse> getProductDetails(
+            @PathVariable Long productId,
+            @RequestHeader("X-User-Agent") Long userId) {
+        return ResponseEntity.ok(productService.getProductDetails(productId, userId));
     }
 }
