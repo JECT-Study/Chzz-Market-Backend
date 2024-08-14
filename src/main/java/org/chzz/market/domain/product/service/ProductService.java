ce034dc54410937477da5b8a7180f3ac96bb26f7
JongHyunJung <jjh19960115@gmail.com>
2024-08-14 23:17:07 +0900

    feat: 사전 등록 상품 상세 정보 조회하는 서비스 로직 추가 (#CHZZ-74)

--- src/main/java/org/chzz/market/domain/product/service/ProductService.java
+++ src/main/java/org/chzz/market/domain/product/service/ProductService.java
@@ -1,8 +1,8 @@
 package org.chzz.market.domain.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.chzz.market.domain.product.dto.ProductDetailsResponse;
 import org.chzz.market.domain.product.dto.ProductResponse;
-import org.chzz.market.domain.product.entity.Product;
 import org.chzz.market.domain.product.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import static org.chzz.market.domain.product.entity.Product.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,13 +20,17 @@ public class ProductService {
     private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
     private final ProductRepository productRepository;
 
-    /**
-     * 카테고리별 상품 목록 조회
+    /*
+     * 카테고리별 사전 등록 상품 목록 조회
      */
-    public Page<ProductResponse> getProductListByCategory(
-            Product.Category category,
-            Long userId,
-            Pageable pageable) {
+    public Page<ProductResponse> getProductListByCategory(Category category, Long userId, Pageable pageable) {
         return productRepository.findProductsByCategory(category, userId, pageable);
+    }
+
+    /*
+     * 상품 상세 정보 조회
+     */
+    public ProductDetailsResponse getProductDetails(Long productId, Long userId) {
+        return productRepository.findProductDetailsById(productId, userId);
     }
 }
