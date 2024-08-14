c906e115e493b1d36014ebd8ada3cc167da2df2f
JongHyunJung <jjh19960115@gmail.com>
2024-08-14 19:23:34 +0900

    feat: 카테고리 별 상품 목록 조회 API 구현 (#CHZZ-75)

--- src/main/java/org/chzz/market/domain/product/controller/ProductController.java
+++ src/main/java/org/chzz/market/domain/product/controller/ProductController.java
@@ -1,6 +1,10 @@
 package org.chzz.market.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.chzz.market.domain.product.entity.Product.Category;
+import org.chzz.market.domain.product.service.ProductService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,4 +15,18 @@ import org.slf4j.LoggerFactory;
 public class ProductController {
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
+    private final ProductService productService;
+
+    /**
+     * 사전 등록 상품 목록 조회
+     */
+    // TODO: 추후에 인증된 사용자 정보로 수정 필요
+    @GetMapping
+    public ResponseEntity<?> getProductList(
+            @RequestParam Category category,
+//            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestHeader("X-User-Agent") Long userId,
+            Pageable pageable) {
+        return ResponseEntity.ok(productService.getProductListByCategory(category, userId, pageable)); // 임의의 사용자 ID
+    }
 }
