efe6e9fdf4ed2e4812abbd642039cb9da4cc0960
junest66 <chlwndks333@gmail.com>
2024-08-14 15:24:14 +0900

    refactor: getOrderSpecifiers 함수 가독성 향상을 위해 리팩토링
    (#61)

--- src/main/java/org/chzz/market/common/util/QuerydslOrderProvider.java
+++ src/main/java/org/chzz/market/common/util/QuerydslOrderProvider.java
@@ -1,39 +1,40 @@
 package org.chzz.market.common.util;
 
+import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class QuerydslOrderProvider {
     private final QuerydslOrderRegistry querydslOrderRegistry;
+
     public Optional<QuerydslOrder> getByName(String name) {
         return Optional.of(querydslOrderRegistry.getOrderByName(name));
     }
 
+    public OrderSpecifier<?>[] getOrderSpecifiers(Pageable pageable) {
+        List<OrderSpecifier<?>> orderSpecifiers = pageable.getSort().stream()
+                .map(this::createOrderSpecifier)
+                .flatMap(Optional::stream)
+                .collect(Collectors.toList());
 
-    public OrderSpecifier[] getOrderSpecifiers(Pageable pageable) {
-        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
-        if (!pageable.getSort().isEmpty()) {
-            for (Order order : pageable.getSort()) {
-                com.querydsl.core.types.Order direction =
-                        order.getDirection().isAscending()
-                                ? com.querydsl.core.types.Order.ASC
-                                : com.querydsl.core.types.Order.DESC;
-
-                getByName(order.getProperty()).ifPresent(querydslOrder ->
-                        orderSpecifiers
-                                .add(new OrderSpecifier<>(direction, querydslOrder.getComparableExpressionBase())));
-            }
-        } else {
+        if (orderSpecifiers.isEmpty()) {
             orderSpecifiers.add(OrderByNull.DEFAULT);
         }
+
         return orderSpecifiers.toArray(OrderSpecifier[]::new);
+    }
+
+    private Optional<OrderSpecifier<?>> createOrderSpecifier(Sort.Order order) {
+        Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+        return getByName(order.getProperty())
+                .map(querydslOrder -> new OrderSpecifier<>(direction, querydslOrder.getComparableExpressionBase()));
     }
 }
