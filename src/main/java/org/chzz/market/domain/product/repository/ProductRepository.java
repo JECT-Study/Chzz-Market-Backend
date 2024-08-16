ffb1b9751bba62d6aea49761e0d7ca340ca92a49
JongHyunJung <jjh19960115@gmail.com>
2024-08-16 10:45:10 +0900

    feat: 상품 repository 쿼리 메서드 추가 (#CHZZ-78) - 경매로 등록되지 않은 상품 조회

--- src/main/java/org/chzz/market/domain/product/repository/ProductRepository.java
+++ src/main/java/org/chzz/market/domain/product/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package org.chzz.market.domain.product.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.chzz.market.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+    @Query("SELECT p FROM Product p WHERE p.id = :id AND NOT EXISTS (SELECT a FROM Auction a WHERE a.product = p)")
+    Optional<Product> findProductForLike(@Param("id") Long id);
 }
