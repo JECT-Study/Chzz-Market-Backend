0709d283f640f818c79d6354015df0638d2909a8
junest66 <chlwndks333@gmail.com>
2024-08-24 23:33:16 +0900

    feat: 이미 소셜로그인을 한 적이 있는 유저인지 확인하는 쿼리 추가
    (#84)

--- src/main/java/org/chzz/market/domain/user/repository/UserRepository.java
+++ src/main/java/org/chzz/market/domain/user/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package org.chzz.market.domain.user.repository;
 
+import java.util.Optional;
 import org.chzz.market.domain.user.entity.User;
+import org.chzz.market.domain.user.entity.User.ProviderType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderTypeAndProviderId(ProviderType providerType, String providerId);
 }
