fa40e93b8d4bf0197243c58f6d1ceb66943cb0a2
junest66 <chlwndks333@gmail.com>
2024-08-25 03:00:43 +0900

    refactor: LoginUserArgumentResolver 인증되지 않은 사용자 null 처리
    (#84)

--- src/main/java/org/chzz/market/common/config/LoginUserArgumentResolver.java
+++ src/main/java/org/chzz/market/common/config/LoginUserArgumentResolver.java
@@ -2,6 +2,7 @@ package org.chzz.market.common.config;
 
 import org.chzz.market.domain.user.dto.CustomUserDetails;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -20,9 +21,15 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext()
-                .getAuthentication()
-                .getPrincipal();
-        return userDetails.getUser().getId();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;  // 인증되지 않은 경우 null 반환
+        }
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof CustomUserDetails) {
+            CustomUserDetails userDetails = (CustomUserDetails) principal;
+            return userDetails.getUser().getId();
+        }
+        return null;
     }
 }
