183cc871d7661ef07dac70aff3c8746d9ded0592
junest66 <chlwndks333@gmail.com>
2024-08-24 23:51:56 +0900

    feat: 추가정보를 받아 최종 회원가입을 하는 서비스 함수 추가
    (#84)

---
+++ src/main/java/org/chzz/market/domain/user/service/UserService.java
@@ -0,0 +1,32 @@
+package org.chzz.market.domain.user.service;
+
+import static org.chzz.market.domain.user.error.UserErrorCode.USER_NOT_FOUND;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.chzz.market.domain.user.dto.request.UserCreateRequest;
+import org.chzz.market.domain.user.entity.User;
+import org.chzz.market.domain.user.error.UserErrorCode;
+import org.chzz.market.domain.user.error.exception.UserException;
+import org.chzz.market.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User createUser(Long userId, UserCreateRequest userCreateRequest) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserException(USER_NOT_FOUND));
+        if (userRepository.existsByNickname(userCreateRequest.getNickname())) {
+            throw new UserException(UserErrorCode.NICKNAME_DUPLICATION);
+        }
+        user.createUser(userCreateRequest);
+        user.addBankAccount(userCreateRequest.toBankAccount(user));
+        return user;
+    }
+}
