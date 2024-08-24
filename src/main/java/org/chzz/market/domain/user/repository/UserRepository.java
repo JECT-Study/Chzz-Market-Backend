1493f2b3d4b50a09285a0fb820a7f27b405b45b8
junest66 <chlwndks333@gmail.com>
2024-08-24 23:52:39 +0900

    feat: 닉네임 이용가능을 확인하는 쿼리 추가
    (#84)

--- src/main/java/org/chzz/market/domain/user/repository/UserRepository.java
+++ src/main/java/org/chzz/market/domain/user/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByProviderTypeAndProviderId(ProviderType providerType, String providerId);
+
+    boolean existsByNickname(String nickname);
 }
