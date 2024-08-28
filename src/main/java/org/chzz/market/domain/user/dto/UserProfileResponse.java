e2a57126a800c641d3d5d12a4f6e1ee96b14a29c
JongHyunJung <jjh19960115@gmail.com>
2024-08-29 08:08:07 +0900

    refactor: 유저 프로필 조회에 필요한 칼럼 수정 (#CHZZ-80) - region 컬럼 삭제

--- src/main/java/org/chzz/market/domain/user/dto/UserProfileResponse.java
+++ src/main/java/org/chzz/market/domain/user/dto/UserProfileResponse.java
@@ -4,14 +4,12 @@ import org.chzz.market.domain.user.entity.User;
 
 public record UserProfileResponse (
         String nickname,
-        String region,
         String bio,
         ParticipationCountsResponse participationCount
 ) {
     public static UserProfileResponse of (User user, ParticipationCountsResponse counts) {
         return new UserProfileResponse(
                 user.getNickname(),
-                user.getRegion(),
                 user.getBio(),
                 counts
         );
