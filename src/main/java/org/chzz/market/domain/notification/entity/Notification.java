4f83650d22587bc6f6b969d485d0b9a07d2c44df
junest66 <chlwndks333@gmail.com>
2024-08-30 18:01:25 +0900

    feat: 알림 Entity에 제품, 삭제 여부 컬럼 추가
    (#85)

--- src/main/java/org/chzz/market/domain/notification/entity/Notification.java
+++ src/main/java/org/chzz/market/domain/notification/entity/Notification.java
@@ -17,6 +17,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.chzz.market.domain.base.entity.BaseTimeEntity;
+import org.chzz.market.domain.notification.error.NotificationErrorCode;
+import org.chzz.market.domain.notification.error.NotificationException;
+import org.chzz.market.domain.product.entity.Product;
 import org.chzz.market.domain.user.entity.User;
 
 @Getter
@@ -35,23 +38,48 @@ public class Notification extends BaseTimeEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
     @Column(nullable = false)
     private String message;
 
     @Column(nullable = false)
     private boolean isRead;
 
+    @Column(nullable = false)
+    private boolean isDeleted;
+
     @Column(nullable = false, columnDefinition = "varchar(30)")
     @Enumerated(EnumType.STRING)
     private Type type;
 
+    public void read() {
+        if(this.isRead) {
+            throw new NotificationException(NotificationErrorCode.ALREADY_READ_NOTIFICATION);
+        }
+        if (this.isDeleted) {
+            throw new NotificationException(NotificationErrorCode.DELETED_NOTIFICATION);
+        }
+        this.isRead = true;
+    }
+
+    public void delete() {
+        if (this.isDeleted) {
+            throw new NotificationException(NotificationErrorCode.DELETED_NOTIFICATION);
+        }
+        this.isDeleted = true;
+    }
+
     @AllArgsConstructor
     public enum Type {
-        AUCTION_START("좋아요를 누르신 사전등록 '%s'의 경매가 시작되었습니다."),
+        AUCTION_START("좋아요를 누르신 사전 등록 제품 '%s'의 경매가 시작되었습니다."),
         AUCTION_SUCCESS("경매에 올린 '%s'가 낙찰되었습니다."),
         AUCTION_FAILURE("경매에 올린 '%s'가 미낙찰되었습니다."),
         AUCTION_WINNER("축하합니다! 입찰에 참여한 경매 '%s'의 낙찰자로 선정되었습니다."),
-        AUCTION_NON_WINNER("안타깝지만 입찰에 참여한 경매 '%s'에 낙찰되지 못했습니다.");
+        AUCTION_NON_WINNER("안타깝지만 입찰에 참여한 경매 '%s'에 낙찰되지 못했습니다."),
+        AUCTION_REGISTRATION_CANCELED("좋아요를 누른 사전 등록 제품 '%s'이(가) 판매자에 의해 취소되었습니다.");
 
         private final String message;
 
