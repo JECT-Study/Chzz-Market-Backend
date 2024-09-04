a1bda27e5d40e4fd85842af144f4939a96101ce8
junest66 <chlwndks333@gmail.com>
2024-09-05 01:45:26 +0900

    refactor: 상품의 소유자 확인하는 함수 적용
    (#86)

--- src/main/java/org/chzz/market/domain/auction/service/AuctionService.java
+++ src/main/java/org/chzz/market/domain/auction/service/AuctionService.java
@@ -3,11 +3,11 @@ package org.chzz.market.domain.auction.service;
 import static org.chzz.market.domain.auction.error.AuctionErrorCode.AUCTION_ALREADY_REGISTERED;
 import static org.chzz.market.domain.auction.error.AuctionErrorCode.AUCTION_NOT_ACCESSIBLE;
 import static org.chzz.market.domain.auction.error.AuctionErrorCode.AUCTION_NOT_FOUND;
-import static org.chzz.market.domain.auction.error.AuctionErrorCode.FORBIDDEN_AUCTION_ACCESS;
 import static org.chzz.market.domain.notification.entity.Notification.Type.AUCTION_FAILURE;
 import static org.chzz.market.domain.notification.entity.Notification.Type.AUCTION_NON_WINNER;
 import static org.chzz.market.domain.notification.entity.Notification.Type.AUCTION_SUCCESS;
 import static org.chzz.market.domain.notification.entity.Notification.Type.AUCTION_WINNER;
+import static org.chzz.market.domain.product.error.ProductErrorCode.FORBIDDEN_PRODUCT_ACCESS;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +28,7 @@ import org.chzz.market.domain.notification.dto.NotificationMessage;
 import org.chzz.market.domain.notification.service.NotificationService;
 import org.chzz.market.domain.product.entity.Product;
 import org.chzz.market.domain.product.entity.Product.Category;
+import org.chzz.market.domain.product.error.ProductException;
 import org.chzz.market.domain.product.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -84,8 +85,8 @@ public class AuctionService {
                 .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
 
         // 등록된 상품의 사용자 정보와 전환 요청한 사용자 정보 유효성 검사
-        if (!product.getUser().getId().equals(userId)) {
-            throw new AuctionException(FORBIDDEN_AUCTION_ACCESS);
+        if (!product.isOwner(userId)) {
+            throw new ProductException(FORBIDDEN_PRODUCT_ACCESS);
         }
 
         // 이미 경매로 등록된 상품인지 유효성 검사
