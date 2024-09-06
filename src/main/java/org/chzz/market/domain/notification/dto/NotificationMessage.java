5ff124bf3cc0f5784f22fa3758a9a9ee1407fc13
junest66 <chlwndks333@gmail.com>
2024-09-06 15:01:45 +0900

    refactor: NotificationMessage 필드 Product -> Image로 변경
    (#85)

--- src/main/java/org/chzz/market/domain/notification/dto/NotificationMessage.java
+++ src/main/java/org/chzz/market/domain/notification/dto/NotificationMessage.java
@@ -1,50 +1,31 @@
 package org.chzz.market.domain.notification.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.chzz.market.domain.image.entity.Image;
 import org.chzz.market.domain.notification.entity.NotificationType;
-import org.chzz.market.domain.product.entity.Product;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class NotificationMessage {
     private List<Long> userIds = new ArrayList<>();
-    private String message;
     private NotificationType type;
+    private String message;
+    private Image image;
 
-    @JsonIgnore
-    private Product product;
-
-    public NotificationMessage(List<Long> userIds, NotificationType type, Product product) {
+    public NotificationMessage(List<Long> userIds, NotificationType type, String message, Image image) {
         this.userIds = userIds;
         this.type = type;
-        this.product = product;
-        this.message = type.getMessage(product.getName());
+        this.message = message;
+        this.image = image;
     }
 
-    public NotificationMessage(Long userId, NotificationType type, Product product) {
+    public NotificationMessage(Long userId, NotificationType type, String message, Image image) {
         this.userIds.add(userId);
         this.type = type;
-        this.product = product;
-        this.message = type.getMessage(product.getName());
-    }
-
-    public NotificationMessage(List<Long> userIds, NotificationType type,
-                               String productName) { // TODO: 사전등록 취소 알림을 위해 임시 작성 (소프트 딜리트로 변경시 삭제)
-        this.userIds = userIds;
-        this.type = type;
-        this.message = type.getMessage(productName);
-    }
-
-    public NotificationMessage(Long userId, NotificationType type,
-                               String productName) { // TODO: 사전등록 취소 알림을 위해 임시 작성 (소프트 딜리트로 변경시 삭제)
-        this.userIds.add(userId);
-        this.type = type;
-        this.message = type.getMessage(productName);
+        this.message = message;
+        this.image = image;
     }
 }
