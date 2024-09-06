3b6a1a46ba47f2f1aa7722ad55259c072b05c525
JongHyunJung <jjh19960115@gmail.com>
2024-09-06 23:07:11 +0900

    refactor: 사용자 프로필 조회 카운트 응답 DTO 수정 (#CHZZ-80)

--- src/main/java/org/chzz/market/domain/user/dto/response/ParticipationCountsResponse.java
+++ src/main/java/org/chzz/market/domain/user/dto/response/ParticipationCountsResponse.java
@@ -3,10 +3,10 @@ package org.chzz.market.domain.user.dto.response;
 import com.querydsl.core.annotations.QueryProjection;
 
 public record ParticipationCountsResponse(
-        long preRegistrationCount,
-        long successfulBidCount,
-        long failedBidCount,
-        long endedAuctionCount
+        long ongoingAuctionCount,
+        long successfulAuctionCount,
+        long failedAuctionCount,
+        long unsuccessfulAuctionCount
 ) {
     @QueryProjection
     public ParticipationCountsResponse {}
