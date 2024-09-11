3aa2452c51656b031cc6cbe8495de6402b0dc2d2
YeaChan05 <qkenrdl05@gmail.com>
2024-09-11 18:43:04 +0900

    [fix] 생성 메서드 추가 (#CHZZ-95)
    생성 위임 메서드 추가

--- src/main/java/org/chzz/market/domain/address/entity/Address.java
+++ src/main/java/org/chzz/market/domain/address/entity/Address.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.chzz.market.domain.address.dto.request.AddressDto;
 import org.chzz.market.domain.base.entity.BaseTimeEntity;
 import org.chzz.market.domain.user.entity.User;
 
@@ -44,4 +45,20 @@ public class Address extends BaseTimeEntity {
 
     @Column
     private String detailAddress;
+
+    public static Address toEntity(User user, AddressDto dto) {
+        return Address.of(user, dto.roadAddress(), dto.jibun(), dto.zipcode(), dto.detailAddress());
+    }
+
+    private static Address of(User user, String roadAddress, String jibun, String zipcode, String detailAddress) {
+        return new Address(user, roadAddress, jibun, zipcode, detailAddress);
+    }
+
+    private Address(User user, String roadAddress, String jibun, String zipcode, String detailAddress) {
+        this.user = user;
+        this.roadAddress = roadAddress;
+        this.jibun = jibun;
+        this.zipcode = zipcode;
+        this.detailAddress = detailAddress;
+    }
 }
