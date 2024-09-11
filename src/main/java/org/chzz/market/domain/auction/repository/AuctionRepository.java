9395321ab22947491cd979aff74d32e6d6ceb073
JongHyunJung <jjh19960115@gmail.com>
2024-09-11 13:22:55 +0900

    feat: 경매 간단 상세 정보 Service 검증 쿼리 추가 (#CHZZ-93)

--- src/main/java/org/chzz/market/domain/auction/repository/AuctionRepository.java
+++ src/main/java/org/chzz/market/domain/auction/repository/AuctionRepository.java
@@ -1,12 +1,9 @@
 package org.chzz.market.domain.auction.repository;
 
 import org.chzz.market.domain.auction.entity.Auction;
-import org.chzz.market.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface AuctionRepository extends JpaRepository<Auction, Long>, AuctionRepositoryCustom {
-    Optional<Auction> findByProduct(Product product);
+    boolean existsByAuctionIdAndUserId(Long auctionId, Long userId);
     boolean existsByProductId(Long productId);
 }
