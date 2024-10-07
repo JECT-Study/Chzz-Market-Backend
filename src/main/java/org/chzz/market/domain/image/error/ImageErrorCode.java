869dfb24498d8533f6af4b4355d96fed92b64e6a
JongHyunJung <jjh19960115@gmail.com>
2024-10-07 17:29:18 +0900

    refactor: 상품 이미지 처리 비즈니스 에러코드 수정
    - 이미지 개수 5개 넘어갈 때 발생하는 에러코드
    - 최종 상품 이미지가 비어있을 때 발생하는 에러코드
    
    CHZZ-127

--- src/main/java/org/chzz/market/domain/image/error/ImageErrorCode.java
+++ src/main/java/org/chzz/market/domain/image/error/ImageErrorCode.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum ImageErrorCode implements ErrorCode {
     IMAGE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드를 실패했습니다."),
     IMAGE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 삭제를 실패했습니다. "),
-    IMAGE_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 저장을 실패했습니다."),
     INVALID_IMAGE_EXTENSION(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 확장자입니다."),
-    IMAGE_MOVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 이동을 실패했습니다."),
-    INVALID_IMAGE_URL(HttpStatus.BAD_REQUEST, "유효하지 않은 이미지 URL 입니다.");
+    MAX_IMAGE_COUNT_EXCEEDED(HttpStatus.BAD_REQUEST, "이미지는 최대 5개까지 등록할 수 있습니다."),
+    NO_IMAGES_PROVIDED(HttpStatus.BAD_REQUEST, "이미지가 제공되지 않았습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
