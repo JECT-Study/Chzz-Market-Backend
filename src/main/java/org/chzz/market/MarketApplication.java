6f55847c5d32b32540d335b8bdc027f7c4156a40
JongHyunJung <jjh19960115@gmail.com>
2024-10-07 18:30:47 +0900

    refactor: 사용되지 않는 어노테이션 제거
    CHZZ-127

--- src/main/java/org/chzz/market/MarketApplication.java
+++ src/main/java/org/chzz/market/MarketApplication.java
@@ -1,12 +1,8 @@
 package org.chzz.market;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Slf4j
-@EnableScheduling
 @SpringBootApplication
 public class MarketApplication {
     public static void main(String[] args) {
