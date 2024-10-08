0070cd55cca3fac35e02602df9e6c41296fc941d
YeaChan05 <qkenrdl05@gmail.com>
2024-10-08 19:58:54 +0900

    refactor: 대표 이미지 서브쿼리 메서드 수정
    대표 이미지 서브쿼리 메서드 수정
    
    CHZZ-134

--- src/main/java/org/chzz/market/domain/product/repository/ProductRepositoryCustomImpl.java
+++ src/main/java/org/chzz/market/domain/product/repository/ProductRepositoryCustomImpl.java
@@ -13,7 +13,6 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -65,7 +64,7 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
                         product.likes.size().longValue(),
                         isProductLikedByUser(userId)
                 ))
-                .leftJoin(image).on(image.product.id.eq(product.id).and(image.id.eq(getFirstImageId(product.id))))
+                .leftJoin(image).on(image.product.id.eq(product.id).and(image.id.eq(getFirstImageId())))
                 .groupBy(product.id, product.name, image.cdnPath, product.minPrice)
                 .orderBy(querydslOrderProvider.getOrderSpecifiers(pageable))
                 .offset(pageable.getOffset())
@@ -159,7 +158,7 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
                         like.isNotNull()
                 ))
                 .leftJoin(image).on(image.product.id.eq(product.id)
-                        .and(image.id.eq(getFirstImageId(product.id))))
+                        .and(image.id.eq(getFirstImageId())))
                 .orderBy(querydslOrderProvider.getOrderSpecifiers(pageable))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -196,7 +195,7 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
                         product.likes.size().longValue()
                 ))
                 .leftJoin(image).on(image.product.id.eq(product.id)
-                        .and(image.id.eq(getFirstImageId(product.id))))
+                        .and(image.id.eq(getFirstImageId())))
                 .orderBy(querydslOrderProvider.getOrderSpecifiers(pageable))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -208,11 +207,11 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
     }
 
-    private JPQLQuery<Long> getFirstImageId(NumberPath<Long> productId) {
+    private JPQLQuery<Long> getFirstImageId() {
         QImage imageSub = new QImage("imageSub");
         return JPAExpressions.select(imageSub.id)
                 .from(imageSub)
-                .where(imageSub.product.id.eq(productId)
+                .where(imageSub.product.id.eq(product.id)
                         , imageSub.sequence.eq(Expressions.asNumber(1)));
     }
 
