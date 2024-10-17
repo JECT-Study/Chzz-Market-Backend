c7e8184ac0dce1dd109922ec41eb9d2b58acf986
JongHyunJung <jjh19960115@gmail.com>
2024-10-18 01:02:59 +0900

    feat: 기존 배송지 주소 수정 관련 쿼리 추가
    - 특정 유저 주소지 정보 조회 쿼리 수정
    - 특정 주소 ID, 유저 ID 해당하는 주소지 조회
    - 특정 유저 ID의 모든 주소 기본 배송지 false 수정
    
    #CHZZ-154

--- src/main/java/org/chzz/market/domain/address/repository/AddressRepository.java
+++ src/main/java/org/chzz/market/domain/address/repository/AddressRepository.java
@@ -1,15 +1,24 @@
 package org.chzz.market.domain.address.repository;
 
+import java.util.Optional;
 import org.chzz.market.domain.address.dto.request.AddressDto;
 import org.chzz.market.domain.address.entity.Address;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    @Query("SELECT new org.chzz.market.domain.address.dto.request.AddressDto(a.roadAddress, a.jibun, a.zipcode, a.detailAddress) " +
+    @Query("SELECT new org.chzz.market.domain.address.dto.request.AddressDto(a.roadAddress, a.jibun, a.zipcode, a.detailAddress"
+            + ", a.recipientName, a.phoneNumber, a.deliveryMemo, a.isDefault) " +
             "FROM Address a WHERE a.user.id = :userId")
     Page<AddressDto> findAddressesByUserId(Pageable pageable, @Param("userId") Long userId);
+
+    Optional<Address> findByIdAndUserId(Long addressId, Long userId);
+
+    @Modifying
+    @Query("UPDATE Address a SET a.isDefault = false WHERE a.user.id = :userId")
+    void updateAllDefaultToFalse(@Param("userId") Long userId);
 }
