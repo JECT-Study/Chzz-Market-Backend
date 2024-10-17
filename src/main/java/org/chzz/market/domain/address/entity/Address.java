d325e75807c6d7ae3ca28dc71d535a5de0e2c492
JongHyunJung <jjh19960115@gmail.com>
2024-10-18 00:58:12 +0900

    refactor: Address 엔티티 수정
    - 배송자 이름 필드 추가
    - 휴대폰 번호 필드 추가
    - 배송 메모 필드 추가
    - 기본 배송지 유무 필드 추가
    - 일부 컬럼 NOT NULL 제약 조건 설정
    - update 메소드 추가
    
    #CHZZ-154

--- src/main/java/org/chzz/market/domain/address/entity/Address.java
+++ src/main/java/org/chzz/market/domain/address/entity/Address.java
@@ -31,17 +31,29 @@ public class Address extends BaseTimeEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column
+    @Column(nullable = false)
     private String roadAddress;
 
     @Column
     private String jibun;
 
-    @Column
+    @Column(nullable = false)
     private String zipcode;
 
-    @Column
+    @Column(nullable = false)
     private String detailAddress;
+
+    @Column(nullable = false)
+    private String recipientName;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column
+    private String deliveryMemo;
+
+    @Column(nullable = false)
+    private boolean isDefault;
 
     public static Address toEntity(User user, AddressDto dto) {
         return new Address(user, dto.roadAddress(), dto.jibun(), dto.zipcode(), dto.detailAddress());
@@ -53,5 +65,16 @@ public class Address extends BaseTimeEntity {
         this.jibun = jibun;
         this.zipcode = zipcode;
         this.detailAddress = detailAddress;
+    }
+
+    public void update(AddressDto dto) {
+        this.roadAddress = dto.roadAddress();
+        this.jibun = dto.jibun();
+        this.zipcode = dto.zipcode();
+        this.detailAddress = dto.detailAddress();
+        this.recipientName = dto.recipientName();
+        this.phoneNumber = dto.phoneNumber();
+        this.deliveryMemo = dto.deliveryMemo();
+        this.isDefault = dto.isDefault();
     }
 }
