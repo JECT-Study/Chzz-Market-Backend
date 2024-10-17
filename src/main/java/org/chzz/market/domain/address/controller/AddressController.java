3622c3874ecd8f48af72fedee4eaf5b5a6aa6a05
JongHyunJung <jjh19960115@gmail.com>
2024-10-18 01:03:17 +0900

    feat: 기존 배송지 주소 수정 API 구현
    #CHZZ-154

--- src/main/java/org/chzz/market/domain/address/controller/AddressController.java
+++ src/main/java/org/chzz/market/domain/address/controller/AddressController.java
@@ -7,7 +7,9 @@ import org.chzz.market.domain.address.service.AddressService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,15 +25,25 @@ public class AddressController {
             @LoginUser Long userId,
             Pageable pageable
     ) {
-        return ResponseEntity.ok(addressService.getAddresses(userId,pageable));
+        return ResponseEntity.ok(addressService.getAddresses(userId, pageable));
     }
 
     @PostMapping
     public ResponseEntity<?> addAddress(
             @LoginUser Long userId,
             @RequestBody AddressDto addressDto
-    ){
-        addressService.save(userId,addressDto);
+    ) {
+        addressService.save(userId, addressDto);
         return ResponseEntity.ok().build();//TODO 2024 09 11 16:42:57 : redirect
+    }
+
+    @PutMapping("/{addressId}")
+    public ResponseEntity<Void> updateAddress(
+            @LoginUser Long userId,
+            @PathVariable Long addressId,
+            @RequestBody AddressDto addressDto
+    ) {
+        addressService.updateAddress(userId, addressId, addressDto);
+        return ResponseEntity.ok().build();
     }
 }
