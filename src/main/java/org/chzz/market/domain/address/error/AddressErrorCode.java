831397e10b7d05d3ce241f6dd59ff163eafd69c0
JongHyunJung <jjh19960115@gmail.com>
2024-10-18 03:04:44 +0900

    feat: Address 비즈니스 에러코드 추가
    - 기본 배송지 삭제 시도 시 예외 처리
    
    #CHZZ-154

--- src/main/java/org/chzz/market/domain/address/error/AddressErrorCode.java
+++ src/main/java/org/chzz/market/domain/address/error/AddressErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum AddressErrorCode implements ErrorCode {
-    ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "주소를 찾을 수 없습니다.");
+    ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "주소를 찾을 수 없습니다."),
+    CANNOT_DELETE_DEFAULT_ADDRESS(HttpStatus.BAD_REQUEST, "기본 배송지는 삭제할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
