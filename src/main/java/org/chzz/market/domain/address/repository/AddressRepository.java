df16c4fc6f8e1381c010cb853181bb61124a9929
JongHyunJung <jjh19960115@gmail.com>
2024-10-20 02:31:55 +0900

    refactor: 주소지 Repository 쿼리 메소드 수정
    CHZZ-154

--- src/main/java/org/chzz/market/domain/address/repository/AddressRepository.java
+++ src/main/java/org/chzz/market/domain/address/repository/AddressRepository.java
@@ -1,7 +1,5 @@
 package org.chzz.market.domain.address.repository;
 
-import java.util.Optional;
-import org.chzz.market.domain.address.dto.request.AddressDto;
 import org.chzz.market.domain.address.entity.Address;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,14 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    @Query("SELECT new org.chzz.market.domain.address.dto.request.AddressDto(a.roadAddress, a.jibun, a.zipcode, a.detailAddress, a.isDefault) "
-            +
-            "FROM Address a WHERE a.user.id = :userId")
-    Page<AddressDto> findAddressesByUserId(Pageable pageable, @Param("userId") Long userId);
 
-    Optional<Address> findByIdAndUserId(Long addressId, Long userId);
+    Page<Address> findByUserId(Long userId, Pageable pageable);
 
     @Modifying
-    @Query("UPDATE Address a SET a.isDefault = false WHERE a.user.id = :userId")
-    void updateAllDefaultToFalse(@Param("userId") Long userId);
+    @Query("UPDATE Address a SET a.isDefault = false WHERE a.user.id = :userId AND a.isDefault = true")
+    void updateDefaultToFalse(@Param("userId") Long userId);
+
+    boolean existsByUserId(Long userId);
 }
