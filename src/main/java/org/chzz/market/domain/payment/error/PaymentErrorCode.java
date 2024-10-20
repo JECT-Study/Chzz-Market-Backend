8063546376a2ff84438fde28f8d7a9e4047b32f1
junest66 <chlwndks333@gmail.com>
2024-10-20 23:41:21 +0900

    feat: AOP에 사용할 어노테이션 에러 이름 상수 지정
    CHZZ-157

--- src/main/java/org/chzz/market/domain/payment/error/PaymentErrorCode.java
+++ src/main/java/org/chzz/market/domain/payment/error/PaymentErrorCode.java
@@ -11,6 +11,13 @@ public enum PaymentErrorCode implements ErrorCode {
     INVALID_METHOD(HttpStatus.BAD_REQUEST, "결제 수단이 옳지 않습니다."),
     ALREADY_EXIST(HttpStatus.INTERNAL_SERVER_ERROR, "이미 존재하는 orderId 입니다."),
     CREATION_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "orderId 생성에 실패했습니다. 다시 시도해주세요.");
+
     private final HttpStatus httpStatus;
     private final String message;
+
+    public static class Const {
+        public static final String INVALID_METHOD = "INVALID_METHOD";
+        public static final String ALREADY_EXIST = "ALREADY_EXIST";
+        public static final String CREATION_FAILURE = "CREATION_FAILURE";
+    }
 }
