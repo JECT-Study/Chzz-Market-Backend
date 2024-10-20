e9426cf6753fd6c55918ac0e452fd8c4863d09e4
junest66 <chlwndks333@gmail.com>
2024-10-20 23:53:48 +0900

    refactor: AddressController Interface 적용
    CHZZ-157

--- src/main/java/org/chzz/market/domain/address/controller/AddressController.java
+++ src/main/java/org/chzz/market/domain/address/controller/AddressController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.chzz.market.common.config.LoginUser;
 import org.chzz.market.domain.address.dto.request.AddressDto;
 import org.chzz.market.domain.address.service.AddressService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,23 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/addresses")
 @RequiredArgsConstructor
-public class AddressController {
+public class AddressController implements AddressApi {
     private final AddressService addressService;
 
+    @Override
     @GetMapping
-    public ResponseEntity<?> getAddresses(
+    public ResponseEntity<Page<AddressDto>> getAddresses(
             @LoginUser Long userId,
-            Pageable pageable
-    ) {
-        return ResponseEntity.ok(addressService.getAddresses(userId,pageable));
+            Pageable pageable) {
+        return ResponseEntity.ok(addressService.getAddresses(userId, pageable));
     }
 
+    @Override
     @PostMapping
-    public ResponseEntity<?> addAddress(
+    public ResponseEntity<Void> addAddress(
             @LoginUser Long userId,
             @RequestBody AddressDto addressDto
-    ){
-        addressService.save(userId,addressDto);
+    ) {
+        addressService.save(userId, addressDto);
         return ResponseEntity.ok().build();//TODO 2024 09 11 16:42:57 : redirect
     }
 }
