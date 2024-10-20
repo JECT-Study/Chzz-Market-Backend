467d97b9aafad4a968c8890f5506f8dd5cd090b6
JongHyunJung <jjh19960115@gmail.com>
2024-10-20 14:06:28 +0900

    refactor: 배송지 Repository 메소드 제거
    CHZZ-154

--- src/main/java/org/chzz/market/domain/address/repository/AddressRepository.java
+++ src/main/java/org/chzz/market/domain/address/repository/AddressRepository.java
@@ -5,17 +5,10 @@ import org.chzz.market.domain.address.entity.Address;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     Page<Address> findByUserId(Long userId, Pageable pageable);
-
-    @Modifying
-    @Query("UPDATE Address a SET a.isDefault = false WHERE a.user.id = :userId AND a.isDefault = true")
-    void updateDefaultToFalse(@Param("userId") Long userId);
 
     Optional<Address> findByUserIdAndIsDefaultTrue(Long userId);
 }
