3ec1e331293ac9825575af7c067712a653447725
YeaChan05 <qkenrdl05@gmail.com>
2024-10-21 11:21:54 +0900

    fix: 예약어 충돌로 인해 테이블명 변경
    예약어 충돌로 인해 테이블명 변경
    
    CHZZ-162

--- src/main/java/org/chzz/market/domain/order/entity/Order.java
+++ src/main/java/org/chzz/market/domain/order/entity/Order.java
@@ -6,9 +6,11 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,11 +24,12 @@ import org.chzz.market.domain.payment.entity.Payment.PaymentMethod;
 @Entity
 @Getter
 @Builder
+@Table(name = "orders")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Order {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
     private Long id;
 
