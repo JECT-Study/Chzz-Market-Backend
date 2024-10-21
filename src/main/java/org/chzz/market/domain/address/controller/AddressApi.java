164bd27a775e0ad7648cce8286117894a9713885
junest66 <chlwndks333@gmail.com>
2024-10-21 12:06:07 +0900

    Merge branch 'develop' into feat/spring-doc-openapi with conflict resolution

--- src/main/java/org/chzz/market/domain/address/controller/AddressApi.java
+++ src/main/java/org/chzz/market/domain/address/controller/AddressApi.java
@@ -6,12 +6,13 @@ import org.chzz.market.domain.address.dto.DeliveryRequest;
 import org.chzz.market.domain.address.dto.DeliveryResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "addresses", description = "배송지 API")
 public interface AddressApi {
 
-    @Operation(summary = "배송지 목록 조회")
+    @Operation(summary = "배송지 목록 조회", description = "기본배송지가 먼저 나오고, 최신순으로 정렬합니다.")
     public ResponseEntity<Page<DeliveryResponse>> getAddresses(Long userId, Pageable pageable);
 
     @Operation(summary = "배송지 추가")
