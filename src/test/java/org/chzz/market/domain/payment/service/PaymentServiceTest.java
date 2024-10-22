89845b675a283e1298377e782f2ca62c32ebb587
YeaChan05 <qkenrdl05@gmail.com>
2024-10-22 21:17:56 +0900

    test: 메서드 접근 제한자 수정으로 인한 테스트 코드 수정
    메서드 접근 제한자 수정으로 인한 테스트 코드 수정
    
    CHZZ-166

--- src/test/java/org/chzz/market/domain/payment/service/PaymentServiceTest.java
+++ src/test/java/org/chzz/market/domain/payment/service/PaymentServiceTest.java
@@ -5,11 +5,17 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
+import org.chzz.market.domain.auction.entity.Auction;
+import org.chzz.market.domain.payment.dto.ShippingAddressRequest;
+import org.chzz.market.domain.payment.dto.request.ApprovalRequest;
 import org.chzz.market.domain.payment.dto.response.TossPaymentResponse;
 import org.chzz.market.domain.payment.entity.Payment;
 import org.chzz.market.domain.payment.entity.Status;
 import org.chzz.market.domain.payment.error.PaymentException;
 import org.chzz.market.domain.payment.repository.PaymentRepository;
+import org.chzz.market.domain.user.entity.User;
+import org.chzz.market.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,14 +31,28 @@ class PaymentServiceTest {
     @Mock
     private PaymentRepository paymentRepository;
 
+    @Mock
+    private UserRepository userRepository;
 
     @Test
     @DisplayName("완료된 결제의 경우 예외를 발생시킨다")
     void assertThatValidateOrderIdWillFilterDone() {
         TossPaymentResponse tossPaymentResponse = new TossPaymentResponse();
         tossPaymentResponse.setStatus(Status.DONE);
-        when(paymentRepository.findByPayerIdAndAuctionId(any(),any()))
-                .thenReturn(List.of(Payment.of(any(), tossPaymentResponse,any())));
-        assertThrows(PaymentException.class, () -> paymentService.validateDuplicatePayment(0L, 0L));
+
+        ShippingAddressRequest shippingAddressRequest = new ShippingAddressRequest(1L, "memo");
+        ApprovalRequest approvalRequest = new ApprovalRequest("orderid", "payment", 1000L, 1L, shippingAddressRequest);
+
+        User user = User.builder()
+                .id(1L)
+                .nickname("닉네임")
+                .bio("자기소개")
+                .build();
+        when(userRepository.findById(any()))
+                .thenReturn(Optional.of(user));
+
+        when(paymentRepository.findByPayerIdAndAuctionId(any(), any()))
+                .thenReturn(List.of(Payment.of(user, tossPaymentResponse, any(Auction.class))));
+        assertThrows(PaymentException.class, () -> paymentService.approval(1L, approvalRequest));
     }
 }
