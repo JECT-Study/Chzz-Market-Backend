d4f7763c895428580037e34a4cece49f8cbae10a
YeaChan05 <qkenrdl05@gmail.com>
2024-10-28 13:05:51 +0900

    refactor: 결제 조건 수정
    최소 결제 금액 조건을 1000의 배수 조건으로 수정
    
    CHZZ-176

--- src/main/java/org/chzz/market/domain/payment/dto/request/ApprovalRequest.java
+++ src/main/java/org/chzz/market/domain/payment/dto/request/ApprovalRequest.java
@@ -1,11 +1,11 @@
 package org.chzz.market.domain.payment.dto.request;
 
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
+import org.chzz.market.common.validation.annotation.ThousandMultiple;
 
 public record ApprovalRequest(String orderId,
                               String paymentKey,
-                              @Min(value = 1000, message = "결제금액은 최소 1,000원 이상, 1000의 배수이어야 합니다")
+                              @ThousandMultiple
                               @Max(value = 2_000_000,message = "결제금액은 200만원을 넘을 수 없습니다")
                               Long amount,
                               Long auctionId,
