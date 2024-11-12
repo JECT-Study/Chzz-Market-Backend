b20047883a40013d3d808371ffe59279b846bf1a
junest66 <chlwndks333@gmail.com>
2024-11-12 16:14:24 +0900

    feat: ImageDeleteService 서비스 함수 추가
    CHZZ-181

---
+++ src/main/java/org/chzz/market/domain/imagev2/service/ImageDeleteService.java
@@ -0,0 +1,50 @@
+package org.chzz.market.domain.imagev2.service;
+
+import static org.chzz.market.domain.imagev2.error.ImageErrorCode.IMAGE_DELETE_FAILED;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.chzz.market.domain.image.entity.ImageV2;
+import org.chzz.market.domain.imagev2.error.exception.ImageException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
+public class ImageDeleteService {
+    private final AmazonS3 amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    /**
+     * S3에서 이미지 삭제
+     */
+    public void deleteImages(List<ImageV2> images) {
+        images.stream()
+                .map(ImageV2::getCdnPath)
+                .forEach(this::deleteImage);
+    }
+
+    /**
+     * 단일 이미지 삭제
+     */
+    private void deleteImage(String cdnPath) {
+        try {
+            String key = new URL(cdnPath).getPath().substring(1);
+            amazonS3Client.deleteObject(bucket, key);
+            log.info("S3에서 객체 삭제, Key : {}", key);
+        } catch (AmazonServiceException | MalformedURLException e) {
+            throw new ImageException(IMAGE_DELETE_FAILED);
+        }
+    }
+}
+
