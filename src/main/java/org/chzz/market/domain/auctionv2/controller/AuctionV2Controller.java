d6fa8ac1dfe4f97ac7fd71cdbebe481b29a1fa2d
junest66 <chlwndks333@gmail.com>
2024-11-16 19:41:34 +0900

    refactor: 컨트롤러에 매핑정보 추가

--- src/main/java/org/chzz/market/domain/auctionv2/controller/AuctionV2Controller.java
+++ src/main/java/org/chzz/market/domain/auctionv2/controller/AuctionV2Controller.java
@@ -1,5 +1,6 @@
 package org.chzz.market.domain.auctionv2.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.chzz.market.common.config.LoginUser;
@@ -13,10 +14,13 @@ import org.chzz.market.domain.auctionv2.service.AuctionCategoryService;
 import org.chzz.market.domain.auctionv2.service.AuctionTestService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,23 +31,33 @@ public class AuctionV2Controller implements AuctionV2Api {
     private final AuctionTestService testService;
 
     @Override
-    public ResponseEntity<Page<?>> getAuctionList(Long userId, Category category, AuctionType type, Pageable pageable) {
+    @GetMapping
+    public ResponseEntity<Page<?>> getAuctionList(@LoginUser Long userId,
+                                                  @RequestParam(required = false) Category category,
+                                                  @RequestParam AuctionType type,
+                                                  @PageableDefault(sort = "newest") Pageable pageable) {
         return null;
     }
 
     @Override
+    @GetMapping("/categories")
     public ResponseEntity<List<CategoryResponse>> getCategoryList() {
         return ResponseEntity.ok(auctionCategoryService.getCategories());
     }
 
     @Override
-    public ResponseEntity<Page<?>> getUserAuctionList(Long userId, UserAuctionType type, Pageable pageable) {
+    @GetMapping("/users")
+    public ResponseEntity<Page<?>> getUserAuctionList(@LoginUser Long userId,
+                                                      @RequestParam UserAuctionType type,
+                                                      @PageableDefault(sort = "newest") Pageable pageable) {
         return null;
     }
 
     @Override
-    public ResponseEntity<RegisterResponse> registerAuction(Long userId, BaseRegisterRequest request,
-                                                            List<MultipartFile> images) {
+    @PostMapping
+    public ResponseEntity<RegisterResponse> registerAuction(@LoginUser Long userId,
+                                                            @RequestPart("request") @Valid BaseRegisterRequest request,
+                                                            @RequestPart(value = "images") List<MultipartFile> images) {
         return null;
     }
 
