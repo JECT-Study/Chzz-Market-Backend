34a80ecf9ea160725ca687392de8c497c45a69d4
junest66 <chlwndks333@gmail.com>
2024-11-16 23:49:08 +0900

    test: @Transactional 어노테이션 누락 추가

--- src/test/java/org/chzz/market/domain/auctionv2/repository/AuctionV2QueryRepositoryTest.java
+++ src/test/java/org/chzz/market/domain/auctionv2/repository/AuctionV2QueryRepositoryTest.java
@@ -17,8 +17,10 @@ import org.chzz.market.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class AuctionV2QueryRepositoryTest {
     @Autowired
     private AuctionV2QueryRepository auctionQueryRepository;
