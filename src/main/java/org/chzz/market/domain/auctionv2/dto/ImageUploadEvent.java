1f7ca9671ee651561fb84ee9a217ff93486f2f0f
YeaChan05 <qkenrdl05@gmail.com>
2024-11-19 22:28:57 +0900

    fix: 불필요한 getter 제거
    record에 불필요한 getter 어노테이션 제거

--- src/main/java/org/chzz/market/domain/auctionv2/dto/ImageUploadEvent.java
+++ src/main/java/org/chzz/market/domain/auctionv2/dto/ImageUploadEvent.java
@@ -1,10 +1,8 @@
 package org.chzz.market.domain.auctionv2.dto;
 
 import java.util.List;
-import lombok.Getter;
 import org.chzz.market.domain.auctionv2.entity.AuctionV2;
 import org.springframework.web.multipart.MultipartFile;
 
-@Getter
 public record ImageUploadEvent(AuctionV2 auction, List<MultipartFile> images) {
 }
