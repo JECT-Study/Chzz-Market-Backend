96a58505e3ff9bab91f106c1221f467f547cd31b
junest66 <chlwndks333@gmail.com>
2024-11-20 02:19:00 +0900

    feat: 좋아요 수를 증가 하거나 감소하는 원자적 쿼리 추가

--- src/main/java/org/chzz/market/domain/auctionv2/repository/AuctionV2Repository.java
+++ src/main/java/org/chzz/market/domain/auctionv2/repository/AuctionV2Repository.java
@@ -5,9 +5,18 @@ import org.chzz.market.domain.auction.repository.AuctionRepositoryCustom;
 import org.chzz.market.domain.auctionv2.entity.AuctionStatus;
 import org.chzz.market.domain.auctionv2.entity.AuctionV2;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AuctionV2Repository extends JpaRepository<AuctionV2, Long>, AuctionRepositoryCustom {
     @Query("SELECT a.status FROM AuctionV2 a WHERE a.id = :auctionId")
     Optional<AuctionStatus> findAuctionStatusById(Long auctionId);
+
+    @Modifying
+    @Query("UPDATE AuctionV2 a SET a.likeCount = a.likeCount + 1 WHERE a.id = :auctionId")
+    void incrementLikeCount(Long auctionId);
+
+    @Modifying
+    @Query("UPDATE AuctionV2 a SET a.likeCount = a.likeCount - 1 WHERE a.id = :auctionId")
+    void decrementLikeCount(Long auctionId);
 }
