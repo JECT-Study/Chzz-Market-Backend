dae44262f1065ec146633c6e5d9371e732c34aa8
YeaChan05 <qkenrdl05@gmail.com>
2024-11-20 23:59:38 +0900

    feat: 상품 등록 엔드포인트 구현
    상품 등록 엔드포인트 구현

--- src/main/java/org/chzz/market/domain/auctionv2/controller/AuctionV2Controller.java
+++ src/main/java/org/chzz/market/domain/auctionv2/controller/AuctionV2Controller.java
@@ -4,8 +4,8 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.chzz.market.common.config.LoginUser;
-import org.chzz.market.domain.auction.dto.request.BaseRegisterRequest;
-import org.chzz.market.domain.auction.dto.response.RegisterResponse;
+import org.chzz.market.domain.auctionv2.dto.AuctionRegisterType;
+import org.chzz.market.domain.auctionv2.dto.request.RegisterRequest;
 import org.chzz.market.domain.auctionv2.dto.response.CategoryResponse;
 import org.chzz.market.domain.auctionv2.dto.view.AuctionType;
 import org.chzz.market.domain.auctionv2.dto.view.UserAuctionType;
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,11 +55,14 @@ public class AuctionV2Controller implements AuctionV2Api {
     }
 
     @Override
-    @PostMapping
-    public ResponseEntity<RegisterResponse> registerAuction(@LoginUser Long userId,
-                                                            @RequestPart("request") @Valid BaseRegisterRequest request,
-                                                            @RequestPart(value = "images") List<MultipartFile> images) {
-        return null;
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Void> registerAuction(
+            @LoginUser Long userId,
+            @RequestPart("request") @Valid RegisterRequest request,
+            @RequestPart(value = "images") List<MultipartFile> images) {
+        AuctionRegisterType type = request.auctionRegisterType();
+        type.getService().register(userId, request, images);//요청 타입에 따라 다른 서비스 호출
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Override
